1dfe535d58bacf36b0d2cd91e30bf2e5c640d0cb
Mizukami Seori <it222235@trident.ac.jp>
2024-07-28 16:37:57 +0900

    update/マウス移動の間隔修正

--- move_trace.py
+++ move_trace.py
@@ -4,10 +4,20 @@ import pyautogui as gui
 filename='a.csv'
 
 with open(filename,'r',encoding='utf-8') as f:
+    # 読み込むだけ(読み込むのにそもそも時間かかるからduration)
+    all_lst=[]
     for line in f:
-        lst=line.split(',')
+        lst=line.strip().split(',')
+        all_lst.append(lst)
+
+
+    # 配列から
+    for lst in all_lst:
         event,x,y=lst[0],int(lst[1]),int(lst[2])
         if event=='move':
+            duray=float(lst[3])
+            # gui.moveTo(x,y,duration=duray)
+            print(x,y,duray)
             gui.moveTo(x,y)
         elif event=='left_click':
             gui.click(x,y)
